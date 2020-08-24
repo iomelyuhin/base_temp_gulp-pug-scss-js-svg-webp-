5ee8a429f68a182ca000750041a8691bb3513741
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2020-08-24 12:19:52 +0300

    correct svg-icons dest

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -7,22 +7,24 @@ const replace = require("gulp-replace");
 module.exports = function svgSprite() {
   return gulp
     .src("src/assets/img/icons/*.svg")
-    // .pipe(
-    //   cheerio({
-    //     run: function ($) {
-    //       $("[fill]").removeAttr("fill");
-    //       $("[stroke]").removeAttr("stroke");
-    //       $("[style]").removeAttr("style");
-    //     },
-    //     parserOptions: { xmlMode: true },
-    //   })
-    // )
-    // .pipe(replace("&gt;", ">"))
+    .pipe(
+      cheerio({
+        run: function ($) {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke");
+          $("[style]").removeAttr("style");
+        },
+        parserOptions: { xmlMode: true },
+      })
+    )
+    .pipe(replace("&gt;", ">"))
     .pipe(
       svgstore({
         inlineSvg: true,
       })
     )
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/assets/img"));
+		.pipe(gulp.dest("build/assets/img"))
+		.pipe(gulp.src("src/assets/img/icons/*.svg"))
+		.pipe(gulp.dest("build/assets/img/icons"));
 };
